f53bce637446024661e723cf974428ba1d488837
robertmichael092 <robertmichael092@gmail.com>
2025-12-18 23:01:24 +0300

    Update db.js

--- db.js
+++ db.js
@@ -2,29 +2,32 @@ let db;
 
 function openDB() {
   return new Promise((resolve) => {
-    const request = indexedDB.open("senti", 1);
+    const req = indexedDB.open("senti-db", 1);
 
-    request.onupgradeneeded = () => {
-      db = request.result;
+    req.onupgradeneeded = e => {
+      db = e.target.result;
       db.createObjectStore("entries", { keyPath: "id", autoIncrement: true });
     };
 
-    request.onsuccess = () => {
-      db = request.result;
+    req.onsuccess = e => {
+      db = e.target.result;
       resolve();
     };
   });
 }
 
 function addEntry(entry) {
-  const tx = db.transaction("entries", "readwrite");
-  tx.objectStore("entries").add(entry);
+  return new Promise(resolve => {
+    const tx = db.transaction("entries", "readwrite");
+    tx.objectStore("entries").add(entry);
+    tx.oncomplete = resolve;
+  });
 }
 
 function getAllEntries() {
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     const tx = db.transaction("entries", "readonly");
     const req = tx.objectStore("entries").getAll();
     req.onsuccess = () => resolve(req.result);
   });
-                     }
+}

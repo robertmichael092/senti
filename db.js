f3599f2d6e3ce28f9f45e44aca5402a9bdd58dfa
robertmichael092 <robertmichael092@gmail.com>
2025-12-18 22:00:10 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,30 @@
+let db;
 
+function openDB() {
+  return new Promise((resolve) => {
+    const request = indexedDB.open("senti", 1);
+
+    request.onupgradeneeded = () => {
+      db = request.result;
+      db.createObjectStore("entries", { keyPath: "id", autoIncrement: true });
+    };
+
+    request.onsuccess = () => {
+      db = request.result;
+      resolve();
+    };
+  });
+}
+
+function addEntry(entry) {
+  const tx = db.transaction("entries", "readwrite");
+  tx.objectStore("entries").add(entry);
+}
+
+function getAllEntries() {
+  return new Promise((resolve) => {
+    const tx = db.transaction("entries", "readonly");
+    const req = tx.objectStore("entries").getAll();
+    req.onsuccess = () => resolve(req.result);
+  });
+                     }

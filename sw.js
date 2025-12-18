c1c698b8c91e2dc8c843eec680cd5d33f18fc271
robertmichael092 <robertmichael092@gmail.com>
2025-12-19 01:40:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,32 @@
-self.addEventListener("fetch", () => {});
+const VERSION = 'senti-v3'; // CHANGE VERSION WHENEVER YOU DEPLOY
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(VERSION).then(cache => cache.addAll([
+      './',
+      './index.html',
+      './styles.css',
+      './app.js',
+      './db.js',
+      './manifest.json'
+    ]))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== VERSION).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
